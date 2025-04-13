a6aa3da4c6e87e9a3191712db0ac24fb84883071
Chris Gambrell <christopher@gambrell.info>
2025-04-13 11:09:23 -0400

    Add undo/redo

--- components/blocks/editor-00/plugins.tsx
+++ components/blocks/editor-00/plugins.tsx
@@ -1,9 +1,9 @@
 import { ContentEditable } from '@/components/editor/editor-ui/content-editable'
+import { HistoryToolbarPlugin } from '@/components/editor/plugins/toolbar/history-toolbar-plugin'
 import { ToolbarPlugin } from '@/components/editor/plugins/toolbar/toolbar-plugin'
-import { Button } from '@/components/ui/base/c-button'
 import { LexicalErrorBoundary } from '@lexical/react/LexicalErrorBoundary'
+import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin'
 import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin'
-import { Plus } from 'lucide-react'
 import { useState } from 'react'
 
 export function Plugins() {
@@ -21,9 +21,7 @@ export function Plugins() {
 			<ToolbarPlugin>
 				{({ blockType }) => (
 					<div className='vertical-align-middle sticky top-0 z-10 flex gap-2 overflow-auto border-b p-1'>
-						<Button variant='outline' size='icon' className='h-8 w-8'>
-							<Plus />
-						</Button>
+						<HistoryToolbarPlugin />
 					</div>
 				)}
 			</ToolbarPlugin>
@@ -39,7 +37,9 @@ export function Plugins() {
 					}
 					ErrorBoundary={LexicalErrorBoundary}
 				/>
+
 				{/* editor plugins */}
+				<HistoryPlugin />
 			</div>
 			{/* actions plugins */}
 		</div>
