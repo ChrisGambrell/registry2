2e364b4a6c7bb92b53547a5da8ced099c18b4c6b
Chris Gambrell <christopher@gambrell.info>
2025-04-13 11:06:38 -0400

    Add toolbar

--- app/editor-00/page.tsx
+++ app/editor-00/page.tsx
@@ -1,47 +1,44 @@
 'use client'
 
-import { useState } from 'react';
-import { SerializedEditorState } from 'lexical';
-import { Editor } from '@/components/blocks/editor-00/editor';
+import { Editor } from '@/components/blocks/editor-00/editor'
+import { SerializedEditorState } from 'lexical'
+import { useState } from 'react'
 
 export const initialValue = {
-  root: {
-    children: [
-      {
-        children: [
-          {
-            detail: 0,
-            format: 0,
-            mode: 'normal',
-            style: '',
-            text: 'Hello World 🚀',
-            type: 'text',
-            version: 1,
-          },
-        ],
-        direction: 'ltr',
-        format: '',
-        indent: 0,
-        type: 'paragraph',
-        version: 1,
-      },
-    ],
-    direction: 'ltr',
-    format: '',
-    indent: 0,
-    type: 'root',
-    version: 1,
-  },
+	root: {
+		children: [
+			{
+				children: [
+					{
+						detail: 0,
+						format: 0,
+						mode: 'normal',
+						style: '',
+						text: 'Hello World 🚀',
+						type: 'text',
+						version: 1,
+					},
+				],
+				direction: 'ltr',
+				format: '',
+				indent: 0,
+				type: 'paragraph',
+				version: 1,
+			},
+		],
+		direction: 'ltr',
+		format: '',
+		indent: 0,
+		type: 'root',
+		version: 1,
+	},
 } as unknown as SerializedEditorState
 
 export default function EditorPage() {
-  const [editorState, setEditorState] =
-    useState<SerializedEditorState>(initialValue)
-  return (
-    <Editor
-      editorSerializedState={editorState}
-      onSerializedChange={(value) => setEditorState(value)}
-    />
-  )
+	const [editorState, setEditorState] = useState<SerializedEditorState>(initialValue)
+	return (
+		<div className='container mx-auto my-4'>
+			<Editor editorSerializedState={editorState} onSerializedChange={(value) => setEditorState(value)} />
+		</div>
+	)
 }
-
--- components/blocks/editor-00/plugins.tsx
+++ components/blocks/editor-00/plugins.tsx
@@ -1,35 +1,47 @@
-import { useState } from 'react';
-import { LexicalErrorBoundary } from '@lexical/react/LexicalErrorBoundary';
-import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
-import { ContentEditable } from '@/components/editor/editor-ui/content-editable';
+import { ContentEditable } from '@/components/editor/editor-ui/content-editable'
+import { ToolbarPlugin } from '@/components/editor/plugins/toolbar/toolbar-plugin'
+import { Button } from '@/components/ui/base/c-button'
+import { LexicalErrorBoundary } from '@lexical/react/LexicalErrorBoundary'
+import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin'
+import { Plus } from 'lucide-react'
+import { useState } from 'react'
 
 export function Plugins() {
-  const [floatingAnchorElem, setFloatingAnchorElem] =
-    useState<HTMLDivElement | null>(null);
+	const [floatingAnchorElem, setFloatingAnchorElem] = useState<HTMLDivElement | null>(null)
 
-  const onRef = (_floatingAnchorElem: HTMLDivElement) => {
-    if (_floatingAnchorElem !== null) {
-      setFloatingAnchorElem(_floatingAnchorElem);
-    }
-  };
+	const onRef = (_floatingAnchorElem: HTMLDivElement) => {
+		if (_floatingAnchorElem !== null) {
+			setFloatingAnchorElem(_floatingAnchorElem)
+		}
+	}
 
-  return (
-    <div className="relative">
-      {/* toolbar plugins */}
-      <div className="relative">
-        <RichTextPlugin
-          contentEditable={
-            <div className="">
-              <div className="" ref={onRef}>
-                <ContentEditable placeholder={'Start typing ...'} />
-              </div>
-            </div>
-          }
-          ErrorBoundary={LexicalErrorBoundary}
-        />
-        {/* editor plugins */}
-      </div>
-      {/* actions plugins */}
-    </div>
-  );
+	return (
+		<div className='relative'>
+			{/* toolbar plugins */}
+			<ToolbarPlugin>
+				{({ blockType }) => (
+					<div className='vertical-align-middle sticky top-0 z-10 flex gap-2 overflow-auto border-b p-1'>
+						<Button variant='outline' size='icon' className='h-8 w-8'>
+							<Plus />
+						</Button>
+					</div>
+				)}
+			</ToolbarPlugin>
+
+			<div className='relative'>
+				<RichTextPlugin
+					contentEditable={
+						<div className=''>
+							<div className='' ref={onRef}>
+								<ContentEditable placeholder={'Start typing ...'} />
+							</div>
+						</div>
+					}
+					ErrorBoundary={LexicalErrorBoundary}
+				/>
+				{/* editor plugins */}
+			</div>
+			{/* actions plugins */}
+		</div>
+	)
 }
