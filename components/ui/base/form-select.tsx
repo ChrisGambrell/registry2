a63a3cc92ddc0f80a0cfa6f59c7ace2544588e39
Chris Gambrell <christopher@gambrell.info>
2025-04-14 15:36:38 -0400

    Add form select

---
+++ components/ui/base/form-select.tsx
@@ -0,0 +1,46 @@
+import { cn } from '@/lib/utils'
+import { SelectProps } from '@radix-ui/react-select'
+import { Label } from '../label'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '../select'
+import { FormError } from './form-error'
+import { ActionState } from './types'
+
+export function FormSelect({
+	className,
+	// clearOnError = false,
+	label,
+	name,
+	options,
+	state,
+	...props
+}: SelectProps & {
+	className?: string
+	// clearOnError?: boolean
+	label: React.ReactNode | string
+	options: { label: React.ReactNode; value: string }[]
+	state?: ActionState
+}) {
+	const id = name ?? ''
+	// TODO: Value and clear on error
+	// const value = clearOnError ? '' : state?.values?.[id] ?? ''
+	const error = state?.fieldErrors?.[id]?.[0]
+
+	return (
+		<div className={cn('grid gap-2 h-fit', className)}>
+			{label && (typeof label === 'string' ? <Label htmlFor={id}>{label}</Label> : label)}
+			<Select {...props}>
+				<SelectTrigger>
+					<SelectValue placeholder='Select an option' />
+				</SelectTrigger>
+				<SelectContent>
+					{options.map((option) => (
+						<SelectItem key={option.value} value={option.value}>
+							{option.label}
+						</SelectItem>
+					))}
+				</SelectContent>
+			</Select>
+			<FormError value={error ? [error] : []} />
+		</div>
+	)
+}
